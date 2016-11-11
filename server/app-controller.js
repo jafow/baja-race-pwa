9cd5326eda150a8fd5a06f1211e67d1c1207f29f
jafow <jared.a.fowler@gmail.com>
2016-11-10 23:20:04 -0800

    add pickFrom helper

--- server/app-controller.js
+++ server/app-controller.js
@@ -1,24 +1,34 @@
 const Pg = require('pg').Pool
-const Pool = new Pg(dbConfig)
+const config = require('./../config.json')
+const tableName = config.table
+const dbpw = config.dbpw
 const dbConfig = {
   host: 'localhost',
   user: 'jaredfowler',
-  password: '',
+  password: dbpw,
   database: 'jaredfowler',
   max: 8,
   idleTimeoutMillis: 28000
 }
-const tableName = require('./config.json').table
+const Pool = new Pg(dbConfig)
+
 
 const appController = function () {
+  var pickFrom = function getFrom (obj) {
+    return function (keyName) {
+      return obj[keyName]
+    }
+  }
+
   return {
     get(req, res, next) {
       Pool.query(`SELECT message from race_messages
           ORDER BY time_received
           DESC
           LIMIT 1`
-          , function (err, data) => {
+          , function (err, data) {
         if (err) {
+          console.error(err)
           res.status(400).render('error',  {data: err})
         }
         req.body.lastUpate = data.rows[0].message
@@ -27,18 +37,24 @@ const appController = function () {
       )
     },
     set(req, res, next) {
-      { msgCode, raceMile, timeStamp, mph } = req.body.parsedMsg
-      Pool.query(`INSERT into ${tableName}
+      var pickFromBody = pickFrom(req.body)
+      // parsedMsg should be attached upstream by msgController
+      var parsed = req.body//Object.assign({}, req.body.parsedMsg)
+      // console.log('parsed id ', parsed)
+      Pool.query(`INSERT into race_messages
           (code, "message", "time_received")
-          VALUES ($1, $2, $3)`, [msgCode, raceMile, timeStamp, mph],
+          VALUES ($1, $2, $3)`,
+          [parsed.msgCode, parsed.raceMile, parsed.timeStamp, parsed.mph],
           function (err, result) {
             if (err) {
+              console.error(err)
               return res.status(400).render('error',  {data: err})
             }
             next()
           }
       )
     }
+  }
 }
 
 module.exports = appController
