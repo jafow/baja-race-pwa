86c8be8b0bf952a9c9a15d0142ad9030c2d0d27e
jafow <jared.a.fowler@gmail.com>
2016-11-11 22:28:27 -0800

    refactor controllers

--- server/app-controller.js
+++ server/app-controller.js
@@ -11,51 +11,48 @@ const dbConfig = {
   idleTimeoutMillis: 28000
 }
 const Pool = new Pg(dbConfig)
+const AppController = Object.create(null)
 
-
-const appController = function () {
-  var pickFrom = function getFrom (obj) {
-    return function (keyName) {
-      return obj[keyName]
+AppController.prototype.get = function (req, res, next) {
+  Pool.query(`SELECT message from race_messages
+      ORDER BY time_received
+      DESC
+      LIMIT 1`
+      , function (err, data) {
+    if (err) {
+      res.status(400).render('error', {data: err})
+      throw err
     }
-  }
 
-  return {
-    get(req, res, next) {
-      Pool.query(`SELECT message from race_messages
-          ORDER BY time_received
-          DESC
-          LIMIT 1`
-          , function (err, data) {
+    req.body.lastUpate = data.rows[0].message
+    next()
+  })
+}
+
+AppController.prototype.set = function (req, res, next) {
+  var pickFromBody = pickFrom(req.body)
+  // parsedMsg should be attached upstream by msgController
+  var parsed = req.body // Object.assign({}, req.body.parsedMsg)
+
+  Pool.query(`INSERT into race_messages
+      (code, "message", "time_received")
+      VALUES ($1, $2, $3)`,
+      [parsed.msgCode, parsed.raceMile, parsed.timeStamp, parsed.mph],
+      function (err, result) {
         if (err) {
           console.error(err)
-          res.status(400).render('error',  {data: err})
+          return res.status(400).render('error',  {data: err})
         }
-        req.body.lastUpate = data.rows[0].message
         next()
-        }
-      )
-    },
-    set(req, res, next) {
-      var pickFromBody = pickFrom(req.body)
-      // parsedMsg should be attached upstream by msgController
-      var parsed = req.body//Object.assign({}, req.body.parsedMsg)
-      // console.log('parsed id ', parsed)
-      Pool.query(`INSERT into race_messages
-          (code, "message", "time_received")
-          VALUES ($1, $2, $3)`,
-          [parsed.msgCode, parsed.raceMile, parsed.timeStamp, parsed.mph],
-          function (err, result) {
-            if (err) {
-              console.error(err)
-              return res.status(400).render('error',  {data: err})
-            }
-            next()
-          }
-      )
-    }
+      }
+    )
+  }
+
+function pickFrom (obj) {
+  return function (keyName) {
+    return obj[keyName]
   }
 }
 
-module.exports = appController
+module.exports = AppController
 
