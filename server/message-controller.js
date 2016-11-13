5df783eb8bd16786053a75fffa9061caecc36621
jafow <jared.a.fowler@gmail.com>
2016-11-12 23:30:09 -0800

    finalize methods, add helpers, cleanup

--- server/message-controller.js
+++ server/message-controller.js
@@ -6,45 +6,67 @@ const twilio = require('twilio')
 
 const client = new twilio.RestClient(sid, token)
 
-const messageController = function () {
-  var extract = function (str) {
-    return function (fn) {
-      return fn(str)
-    }
+function MessageController () {
+}
+
+MessageController.prototype.createMessage = function (req, res, next) {
+  var myMessage = {
+    body: '202',
+    to: toNumber,
+    from: fromNumber
   }
-  // make 3 regexp helpers that find MPH, time seen, race mile 
-  // from the satellite text message
-  return {
-    createMessage(req, res, next) {
-      var myMessage = {
-        body: '202',
-        to: toNumber,
-        from: fromNumber
-      }
 
-      client.messages.create(myMessage, (err, msg) => {
-        if (err) throw err.message
-      })
-      next()
-    },
-    makeData(req, res, next) {
-      var body = req.body.race_mile
-      req.body.renderData = {
-        raceMile: body
-      }
-      next()
-    },
-    parse(req, res, next) {
-      var msgBody = req.body.Body
-      var parsed = {
-        msgCode: // get the code
-        raceMile: extractRaceMile(msgBody),
-        timeStamp: extractTimeStamp
+  client.messages.create(myMessage, (err, msg) => {
+    if (err) throw err.message
+  })
+  next()
+}
 
+MessageController.prototype.parse = function (req, res, next) {
+  var messageBody = req.body.Body
+  var messageBodyList = req.body.Body.split(' ')
+  var smssId = req.body.SmsSid
+  var stripRM = strip(/RM/)
+  var stripEndPd = strip(/\.$/)
+  var strippedRaceMile = compose(stripRM, stripEndPd)
+  var stripMPH = strip(/MPH/)
+  var timeStamp = formatTimeString(messageBodyList[5] + messageBodyList[6])
+  var parsed = {
+    msgCode: smssId,
+    message: messageBody,
+    raceMile: Number(strippedRaceMile(messageBodyList[10])),
+    mph: Number(stripMPH(messageBodyList[8])),
+    lastSeen: `${messageBodyList[4]} ${timeStamp}`
+  }
+
+  req.body.parsed = parsed
+  next()
+}
+
+function strip (removeRgx) {
+  return function (targetString) {
+    return targetString.replace(removeRgx, '')
   }
 }
 
-module.exports = messageController
+function isMorning (timeString) {
+  return /AM/.test(timeString)
+}
 
+function formatTimeString (timeString) {
+  if (isMorning(timeString)) return strip(/AM/)(timeString)
+  else {
+    var h = Number(timeString.substr(0, 2)) + 12
+    var newTimeString = h + timeString.substr(2)
+    return strip(/PM/)(newTimeString)
+  }
+}
 
+function compose (fn1, fn2) {
+  return function (args) {
+    return fn1(fn2(args))
+  }
+}
+
+module.exports = MessageController
 
