92d6017302e1986b5218919fda5bc9a0c30c41b1
jafow <jared.a.fowler@gmail.com>
2016-11-12 23:29:32 -0800

    update querys to match table columns; rename; removed helpers

--- server/db-controller.js
+++ server/db-controller.js
@@ -1,21 +1,22 @@
 const Pg = require('pg').Pool
 const config = require('./../config.json')
-const tableName = config.table
+const tableName = config.tableName
 const dbpw = config.dbpw
 const dbConfig = {
   host: 'localhost',
   user: 'jaredfowler',
   password: dbpw,
   database: 'jaredfowler',
-  max: 8,
+  max: 4,
   idleTimeoutMillis: 28000
 }
 const Pool = new Pg(dbConfig)
-const AppController = Object.create(null)
+function DbController () {
+}
 
-AppController.prototype.get = function (req, res, next) {
-  Pool.query(`SELECT message from race_messages
-      ORDER BY time_received
+DbController.prototype.get = function get (req, res, next) {
+  Pool.query(`SELECT race_mile, mph, last_seen from ${tableName}
+      ORDER BY update_time
       DESC
       LIMIT 1`
       , function (err, data) {
@@ -24,35 +25,28 @@ AppController.prototype.get = function (req, res, next) {
       throw err
     }
 
-    req.body.lastUpate = data.rows[0].message
+    req.body.lastUpate = data.rows[0]
+    console.log('body updated is ', req.body.lastUpate)
     next()
   })
 }
 
-AppController.prototype.set = function (req, res, next) {
-  var pickFromBody = pickFrom(req.body)
-  // parsedMsg should be attached upstream by msgController
-  var parsed = req.body // Object.assign({}, req.body.parsedMsg)
+DbController.prototype.set = function set (req, res, next) {
+  var parsed = req.body.parsed
+  var message = req.body.Body
 
-  Pool.query(`INSERT into race_messages
-      (code, "message", "time_received")
-      VALUES ($1, $2, $3)`,
-      [parsed.msgCode, parsed.raceMile, parsed.timeStamp, parsed.mph],
+  Pool.query(`INSERT into ${tableName}
+      ("msg_code", "message", "race_mile", "mph", "last_seen")
+      VALUES ($1, $2, $3, $4, $5)`,
+      [parsed.msgCode, message, parsed.raceMile, parsed.mph, parsed.lastSeen],
       function (err, result) {
         if (err) {
           console.error(err)
-          return res.status(400).render('error',  {data: err})
+          res.status(400).render('error', {data: err})
         }
         next()
-      }
-    )
-  }
-
-function pickFrom (obj) {
-  return function (keyName) {
-    return obj[keyName]
-  }
+      })
 }
 
-module.exports = AppController
+module.exports = DbController
 
