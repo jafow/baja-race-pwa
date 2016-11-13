d5f4b08552a6442ddb0fc294242ad0078b1ff76e
jafow <jared.a.fowler@gmail.com>
2016-11-12 23:27:47 -0800

    move functionality out of index; create instances of controllers

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var exphbs = require('express-handlebars')
 var path = require('path')
-var messageController = require('./server/message-controller')()
-var appController = require('./server/app-controller')()
+var MessageController = require('./server/message-controller')
+var DbController = require('./server/db-controller')
 var bodyParser = require('body-parser')
-// var uri = `postgresql://jaredfowler:${dbkey}@localhost/jaredfowler`
 const PORT = process.env.PORT || 3001
 var app = express()
+var dbCtrl = new DbController()
+var msgCtrl = new MessageController()
 var hbs = exphbs.create({
   defaultLayout: 'main',
   partialsDir: path.join(__dirname, 'views/partials'),
@@ -18,33 +19,13 @@ app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', appController.get, (req, res) => {
-  res.render('home', req.body)
+app.get('/', dbCtrl.get, (req, res) => {
+  res.render('home', req.body.lastUpdate)
 })
 
-app.post('/sat', appController.set, (req, res) => {
-  var raceMile = req.body.Body
-  var msgCode = Number(req.body.SmsSid.substring(0, 9))
-  var d = formatDateString(new Date())
-
-  Pool.query(`INSERT into race_messages
-      (code, "message", "time_received")
-      VALUES ($1, $2, $3)`, [msgCode, raceMile, d],
-      function (err, result) {
-        if (err) {
-          console.error(err)
-          return res.status(404).send(err)
-        }
-        res.status(200).send('ok\n')
-      }
-  )
+app.post('/sat', msgCtrl.parse, dbCtrl.set, (req, res) => {
+  res.status(200).send('ok\n')
 })
 
 app.listen(PORT, () => console.log(`listening ${PORT}`))
 
-function formatDateString (dateObj) {
-  var d = dateObj || new Date()
-  var ymd = `${(d.getYear()) + 1900}-${(d.getMonth()) + 1}-${(d.getDate())}`
-  var time = `${(d.getHours())}:${d.getMinutes() + 10}:${d.getSeconds()}`
-  return `${ymd} ${time}`
-}
