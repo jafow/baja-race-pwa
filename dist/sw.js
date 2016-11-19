4d70551f818e9e7a50b2b16fd50d82eb017d1f1e
jafow <jared.a.fowler@gmail.com>
2016-11-18 23:27:29 -0800

    setup IDB; fetch and cache stylesheet

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-var DBOpenRequst = window.indexedDB.open('myCache', 1)
+var DBOpenRequest = window.indexedDB.open('myCache', 1)
 var db
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js', {scope: '/dist/'})
@@ -13,16 +13,58 @@ this.addEventListener('install', function (event) {
   event.waitUntil(openAndReadFromDB)
 })
 
-DBOpenRequst.onerror = function (event) {
+DBOpenRequest.onerror = function (event) {
   console.error('event errored: ', event.error)
 }
 
-DBOpenRequst.onsuccess = function (event) {
+DBOpenRequest.onsuccess = function (event) {
   console.log('success')
+}
 
-  db = event.target
+DBOpenRequest.onupgradeneeded = function (event) {
+  db = event.target.result
+
+  var objectStore = db.createObjectStore('caches', {
+    keyPath: 'filePath'
+  })
+
+  objectStore.createIndex('fileName', 'fileName', { unique: true })
+  objectStore.createIndex('img', 'img', { unique: true })
+  objectStore.transaction.oncomplete = function (event) {
+    var filesObjectStore = db
+      .transaction('cacheFiles', 'readwrite')
+      .objectStore('cacheFiles')
+
+    getFileBlob('index.html').then(function (data) {
+      console.log('data', data)
+      filesObjectStore.add({'index.html': data})
+    })
+  }
 }
 
 function openAndReadFromDB () {
   // IDB stuff
 }
+
+function getFileBlob (filePath) {
+  return new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest()
+    req.open('GET', filePath)
+    req.responseType = 'blob'
+
+    req.onload = function () {
+      if (req.status === 200) {
+        resolve(req.response)
+      } else {
+        reject(new Error('error loading: ' + req.statusText))
+      }
+    }
+
+    req.onerror = function () {
+      reject(Error('error in request'))
+    }
+
+    req.send()
+  })
+}
+
